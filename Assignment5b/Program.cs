edc492fa594f226941c7eda0d26aaa597a2b6077
visualstudio <visualstudio@122-PC15>
2019-04-12 18:34:28 -0400

    starter code

--- Assignment5b/Program.cs
+++ Assignment5b/Program.cs
@@ -10,6 +10,28 @@ namespace Assignment5b
     {
         static void Main(string[] args)
         {
+            Program a = new Program();
+            Console.WriteLine("the lomgest word is: ");
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for procesing bt an analytical Engine."));
+
+        }
+        public MyFunctionA(string input)
+        {
+            string[] words = input.Split('');
+            int wordArrayLength = new int[wordArrayLength];
+
+            foreach (var word in words)
+            {
+               
+            }
+            string currentWord = words[0];
+            string nextWord;
+            for (int y = 0; y< words.Length-1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+
+            }
 
         }
     }
