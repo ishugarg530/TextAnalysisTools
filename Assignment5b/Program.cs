120f8f69aeff72dc8f45b909c5d2c0541c5a185f
visualstudio <visualstudio@122-PC15>
2019-04-12 19:38:07 -0400

    Final commit

--- Assignment5b/Program.cs
+++ Assignment5b/Program.cs
@@ -4,34 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestAnalysisTools
+namespace Assignment5b
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Program a = new Program();
+
             Console.WriteLine("the longest word is : ");
+
             Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+
             Console.ReadLine();
         }
 
         public String MyFunctionA(string input)
         {
-            
             string[] words = input.Split(' ');
+
             int wordArrayLength = words.Length;
+
             int[] wordsLength = new int[wordArrayLength];
+
             foreach (var word in words)
-            {           
+            {   
+                
             }
-            
+      
             string currentWord = words[0];
+
             string nextWord;
 
             for (int y = 0; y < words.Length - 1; y++)
             {
                 currentWord = words[y];
+
                 nextWord = words[y + 1];
                 
             }
